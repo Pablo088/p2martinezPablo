680a39996bcd05d7ee32ce74ee51620b3de7ebf6
Pablo088 <129628068+Pablo088@users.noreply.github.com>
2023-04-14 19:20:41 -0300

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,14 @@
+console.log("Funciona")
+alert("Para que entras")
+const aux=document.getElementById("caja")
+alert(aux.ariaValueMax)
+function myfunction(){
+    const dex=getElementById("Algo")
+    alert(dex)
+}
+function que(){
+    const nom=getElementById("nom")
+    const ape=getElementById("ape")
+    const ed=getElementById("ed")
+    alert(nom,ape,ed)
+}
