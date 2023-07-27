854c909bad66484e8075d76c382576b265e295ac
Pablo088 <129628068+Pablo088@users.noreply.github.com>
2023-07-27 19:17:53 -0300

    Add files via upload

---
+++ conectar/conexion.php
@@ -0,0 +1,7 @@
+<?php
+$host = "localhost";
+$dbname = "jugadoras";
+$username = "root";
+$password = null;
+$connection = new PDO('mysql:host=localhost;dbname=jugadoras', $username, $password);
+?>
