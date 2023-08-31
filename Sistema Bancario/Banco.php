ac4dc09ff8967517eedb9201cbb62d8459e442d8
Pablo088 <129628068+Pablo088@users.noreply.github.com>
2023-08-31 20:49:55 -0300

    Banco.php

--- Sistema Bancario/Banco.php
+++ Sistema Bancario/Banco.php
@@ -2,16 +2,16 @@
     class Banco{
         public $nombre;
         public $direccion;
-        public $listaCuentas = array();
+        public $listaCuentas = [];
 
         public function __construct($nombre,$direccion){
             $this -> nombre = $nombre;
             $this -> direccion = $direccion;
     }
-    function AgregarCuenta($cuenta){
-
+    function AgregarCuenta($numeroCuenta){
+        $this-> $listaCuentas[] = $numeroCuenta;
     }
-    function BuscarCuentaPorTitular($cuenta){
+    function BuscarCuentaPorTitular(){
 
     }
 }
