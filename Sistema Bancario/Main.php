564dc059a7c6ccc17ff23181d6f3a986defcea1e
Pablo088 <129628068+Pablo088@users.noreply.github.com>
2023-09-08 16:21:20 -0300

    Main.php

--- Sistema Bancario/Main.php
+++ Sistema Bancario/Main.php
@@ -6,9 +6,21 @@
     $persona1 = new Persona("Leandro","Paredes",29,"37.321.543");
     $persona2 = new Persona("Rodrigo","Puentes",30,"49.432.423");
     $persona3 = new Persona("Fabian","Muros",35,"39.343.432");
-    echo $persona1 -> nombre." ". $persona1->apellido." ". $persona1->edad." ".$persona1->dni;
+    /*echo $persona1 -> nombre." ". $persona1->apellido." ". $persona1->edad." ".$persona1->dni;
     echo $persona2 -> nombre." ". $persona2->apellido." ". $persona2->edad." ".$persona2->dni;
     echo $persona3 -> nombre." ". $persona3->apellido." ". $persona3->edad." ".$persona3->dni;
+*/
+    $banco1 = new Banco("Banco Nacional Peruano","Avellaneda 1500");
+    
+    $cuenta1 = new CuentaBancaria(0010,$persona1->nombre,10000);
+    $cuenta2 = new CuentaBancaria(0015,$persona2->nombre,0);
+    $cuenta3 = new CuentaBancaria(0020,$persona3->nombre,100000);
 
-    $cuenta1 = new CuentaBancaria(0010,$persona1,10000)
+    $banco1 -> agregarCuenta($cuenta1);
+    $banco1 -> agregarCuenta($cuenta2);
+    $banco1 -> agregarCuenta($cuenta3);
+    var_dump($banco1);
+
+    $cuenta1 -> Depositar(1000);
+    //var_dump($cuenta1);
 ?>
