867a2c2dc8c2373e876dd7c47b256e2feb9cf1f2
Pablo088 <129628068+Pablo088@users.noreply.github.com>
2023-09-08 09:44:42 -0300

    Main.php

--- Trait/Main.php
+++ Trait/Main.php
@@ -3,10 +3,12 @@
     require_once("Argentino.php");
    
 
-    $persona1 = new Argentino("Roberto");
+    $persona1 = new Argentino("Roberto","argentino");
+
     
     $persona1->saludar();
     $persona1 -> getNombre();
     $persona1 -> nacionalidad();
-
+    $persona1 -> verNacionalidad($persona1->nacionalidad);
+?>
 ?>
