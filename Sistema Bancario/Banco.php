9cd9b1ed6aa6ed9b6406a061596370a1e054add5
Pablo088 <129628068+Pablo088@users.noreply.github.com>
2023-09-08 16:20:24 -0300

    Banco.php

--- Sistema Bancario/Banco.php
+++ Sistema Bancario/Banco.php
@@ -8,11 +8,22 @@
             $this -> nombre = $nombre;
             $this -> direccion = $direccion;
     }
-    function AgregarCuenta($numeroCuenta){
-        $this-> $listaCuentas[] = $numeroCuenta;
+    function agregarCuenta($numeroCuenta){
+        $this-> listaCuentas[] = $numeroCuenta;
     }
-    function BuscarCuentaPorTitular(){
-
+    function buscarCuentaPorTitular($a,$b){
+        if ($a==($this->listaCuentas[1]->titular->nombre)){
+            echo ("Dentro");
+            var_dump($this->listaCuentas[1]);
+      
+            foreach ($this->listaCuentas as $totalDeCuentas) {
+                //var_dump($cadaCuenta->titular->nombre,$cadaCuenta->titular->apellido);
+                if ((($cadaCuenta->titular->nombre) == $a) && (($cadaCuenta->titular->apellido) == $b) ){
+                    echo("Cuenta Encontrada<br>");
+                    var_dump($cadaCuenta);
+                }
+            }
+        }
     }
 }
 ?>
