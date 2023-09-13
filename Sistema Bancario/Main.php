1eb65b5052ce9bf1bd3dc3e03126ee85402ab771
Pablo088 <129628068+Pablo088@users.noreply.github.com>
2023-09-13 12:38:29 -0300

    Main.php

--- Sistema Bancario/Main.php
+++ Sistema Bancario/Main.php
@@ -9,18 +9,21 @@
     /*echo $persona1 -> nombre." ". $persona1->apellido." ". $persona1->edad." ".$persona1->dni;
     echo $persona2 -> nombre." ". $persona2->apellido." ". $persona2->edad." ".$persona2->dni;
     echo $persona3 -> nombre." ". $persona3->apellido." ". $persona3->edad." ".$persona3->dni;
-*/
+
     $banco1 = new Banco("Banco Nacional Peruano","Avellaneda 1500");
-    
+*/    
     $cuenta1 = new CuentaBancaria(0010,$persona1->nombre,10000);
     $cuenta2 = new CuentaBancaria(0015,$persona2->nombre,0);
     $cuenta3 = new CuentaBancaria(0020,$persona3->nombre,100000);
-
+/*
     $banco1 -> agregarCuenta($cuenta1);
     $banco1 -> agregarCuenta($cuenta2);
     $banco1 -> agregarCuenta($cuenta3);
     var_dump($banco1);
 
     $cuenta1 -> Depositar(1000);
-    //var_dump($cuenta1);
+    var_dump($cuenta1);
+*/
+    $cuenta1 -> Transferir($cuenta3->titular,$cuenta3->saldo,5000,$cuenta1->saldo,$cuenta1->titular);
+    echo $cuenta1;
 ?>
