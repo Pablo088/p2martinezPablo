34487f8ca0cb121e209dcd42b0684cd2da5acd7c
Pablo088 <129628068+Pablo088@users.noreply.github.com>
2023-09-13 14:52:35 -0300

    CuentaBancaria.php

--- Sistema Bancario/CuentaBancaria.php
+++ Sistema Bancario/CuentaBancaria.php
@@ -3,6 +3,7 @@
         public $numeroCuenta;
         public $saldo;
         public $titular;
+        const limite = 20000;
         
         public function __construct($numeroCuenta,$titular,$saldo){
             $this -> numeroCuenta = $numeroCuenta;
@@ -12,18 +13,27 @@
     public function Depositar($cantidad){
         if($cantidad>0){
         $this -> saldo= $this -> saldo += $cantidad;
+        }
     }
     public function Retirar($cantidad){
         if (($this -> saldo)>$cantidad){
-        $this -> saldo=$this ->saldo -= $cantidad;
+        $this -> saldo=$this ->saldo - $cantidad;
+        echo "Retiraste ".$cantidad." y tu saldo ahora es de ".$this->saldo;
+        }else if (($this->saldo)<$cantidad && self::limite>=$cantidad){
+            $this->saldo = $cantidad - self::limite;
+            echo "El monto restante de tu sobregiro es de ".$this->saldo.", por lo que vas a tener que depositar los ".$cantidad." que retiraste, dentro de 30 dias";
+        }else if (($this->saldo)<$cantidad && self::limite<$cantidad){
+            echo"El monto que pedís es mayor al limite de dinero que podes retirar a credito";
+        }
     }
     public function Transferir($nombre1,$transfiereSaldo,$cantidadTransferencia,$recibeSaldo,$nombre2){
         if($transfiereSaldo>0){
             $transfiereSaldo = $transfiereSaldo -= $cantidadTransferencia;
             $recibeSaldo = $recibeSaldo += $cantidadTransferencia;
-           // echo $nombre1." acaba de transferir ".$cantidadTransferencia." pesos a ".$nombre2;
-            echo "Transferencia existosa! ";
-            echo "El saldo de ".$nombre1." es de ".$transfiereSaldo.", y el de ",$nombre2." es de ".$recibeSaldo;
+
+            echo "¡Transferencia existosa! ";
+            echo $nombre1." acaba de transferir ".$cantidadTransferencia." pesos a ".$nombre2;
+            echo ". Ahora, el saldo de ".$nombre1." es de ".$transfiereSaldo.", y el de ",$nombre2." es de ".$recibeSaldo;
         }
     }
 }
