ad7fb2673d78e2f1b99a144a2284f110523791ab
Pablo088 <129628068+Pablo088@users.noreply.github.com>
2023-09-13 20:48:58 -0300

    CuentaBancaria.php

--- Sistema Bancario/CuentaBancaria.php
+++ Sistema Bancario/CuentaBancaria.php
@@ -1,9 +1,11 @@
 <?php
+    require_once("Impuesto_trait.php");
     class CuentaBancaria{
         public $numeroCuenta;
         public $saldo;
         public $titular;
         const limite = 20000;
+        use Impuesto;
         
         public function __construct($numeroCuenta,$titular,$saldo){
             $this -> numeroCuenta = $numeroCuenta;
@@ -11,30 +13,44 @@
             $this -> saldo = $saldo;
     }
     public function Depositar($cantidad){
+        $saldoAumenta = $this->saldo;
+
         if($cantidad>0){
-        $this -> saldo= $this -> saldo += $cantidad;
+        $saldoAumenta= $saldoAumenta + $cantidad;
+        echo "Tu saldo ahora es de ".$saldoAumenta;
         }
+
     }
     public function Retirar($cantidad){
-        if (($this -> saldo)>=$cantidad){
-        $this -> saldo=$this ->saldo - $cantidad;
-        echo "Retiraste ".$cantidad." y tu saldo ahora es de ".$this->saldo;
-        }else if (($this->saldo)<$cantidad && self::limite>=$cantidad){
-            $this->saldo = $cantidad - self::limite;
-            echo "El monto restante de tu sobregiro es de ".$this->saldo.", por lo que vas a tener que depositar los ".$cantidad." que retiraste, dentro de 30 dias";
-        }else if (($this->saldo)<$cantidad && self::limite<$cantidad){
-            echo"El monto que pedís es mayor al limite de dinero que podes retirar a credito";
+        $saldoDisminuye = $this->saldo;
+
+        if (($saldoDisminuye)>=$cantidad){
+        $saldoDisminuye= $saldoDisminuye - $cantidad;
+        echo "Retiraste ".$cantidad." y tu saldo ahora es de ".$saldoDisminuye;
         }
+            else if (($saldoDisminuye)<$cantidad && self::limite>=$cantidad){
+                $saldoDisminuye = $cantidad - self::limite;
+                echo "El monto restante de tu sobregiro es de ".$saldoDisminuye.", por lo que vas a tener que depositar los ".$cantidad." que retiraste, dentro de 30 dias";
+
+            }
+                else if (($saldoDisminuye)<$cantidad && self::limite<$cantidad){
+                    echo"El monto que pedís es mayor al limite de dinero que podes retirar a credito";
+                }
+
     }
+
     public function Transferir($nombre1,$transfiereSaldo,$cantidadTransferencia,$recibeSaldo,$nombre2){
+        
         if($transfiereSaldo>0){
-            $transfiereSaldo = $transfiereSaldo -= $cantidadTransferencia;
-            $recibeSaldo = $recibeSaldo += $cantidadTransferencia;
+            $transfiereSaldo = $transfiereSaldo - $cantidadTransferencia;
+            $recibeSaldo = $recibeSaldo + $cantidadTransferencia;
 
             echo "¡Transferencia existosa! ";
             echo $nombre1." acaba de transferir ".$cantidadTransferencia." pesos a ".$nombre2;
             echo ". Ahora, el saldo de ".$nombre1." es de ".$transfiereSaldo.", y el de ",$nombre2." es de ".$recibeSaldo;
         }
+
     }
+//amongus
 }
 ?>
