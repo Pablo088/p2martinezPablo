35b66298d400e028e74e565ecaac4dce17c4d64f
Pablo088 <129628068+Pablo088@users.noreply.github.com>
2023-09-14 18:45:29 -0300

    Impuesto_trait.php

--- Sistema Bancario/Impuesto_trait.php
+++ Sistema Bancario/Impuesto_trait.php
@@ -1,10 +1,13 @@
 <?php
     trait Impuesto {
-        public function impuestoRetiro(){
-
+        public function impuestoRetiro($saldoRetiro,$cantidad){
+            $saldoRetiro = $saldoRetiro / 0.6;
+            echo "Por el retiro de ".$cantidad." pesos, también se te va a descontar un 0,6% de tu saldo. Tu saldo ahora es de ".$saldoRetiro;
         }
-        public function impuestoTransferencia(){
-            
+
+        public function impuestoTransferencia($saldoTransfiere,$nombre,$cantidad){
+            $saldoTransfiere = $saldoTransfiere / 2;
+            echo $nombre.", por la transferencia de ".$cantidad." pesos realizada también se te va a descontar un 2% de tu saldo. Tu saldo ahora es de ".$saldoTransfiere;
         }
     }
 ?>
