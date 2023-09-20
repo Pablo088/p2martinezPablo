ea41804e33f8a3fccf07125b5ac67ee88e8153c5
Pablo088 <129628068+Pablo088@users.noreply.github.com>
2023-09-20 19:55:18 -0300

    modificacion_alumno.php

---
+++ Sistema de Asistencias/Alumno/Modificacion_Alumno/modificacion_alumno.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Modificar Alumno</title>
+</head>
+<body>
+    <form action="modificacion_alumno.php" method="post">
+        <div>
+            <h3>Ingrese el nombre y apellido del alumno que quiere modificar</h3>
+            <input type="text" name="nombre_alumno_original" placeholder="Nombre original">
+            <input type="text" name="apellido_alumno_original" placeholder="Apellido original">
+        </div>
+        <div>
+        <input type="text" name="dni_alumno" placeholder="DNI">
+            <input type="text" name="nombre_alumno" placeholder="Nombre">
+            <input type="text" name="apellido_alumno" placeholder="Apellido">
+            <input type="text" name="fecha_nacimiento_alumno" placeholder="Fecha de Nacimiento">
+            <input type="submit" value="Modificar">
+        </div>
+    </form>
+</body>
+</html>
+
+<?php
+    require_once('../../../SistemaDeAsistencias/BD/conexion.php');
+
+    $listadoAlumnos = "Select * from alumno";
+    $preparo = $connection -> prepare($listadoAlumnos);
+    $preparo -> execute();
+    $alumnos = $preparo ->fetchAll();
+
+    foreach($alumnos as $listado){
+        echo("<br>");
+        echo($listado["dni_alumno"]);
+        echo" ";
+        echo($listado["nombre_alumno"]);
+        echo" ";
+        echo($listado["apellido_alumno"]);
+        echo" ";
+        echo($listado["fecha_nacimiento_alumno"]);
+        echo" ";
+    }
+
+    if(!empty($_POST["dni_alumno"]) && !empty($_POST["nombre_alumno"]) && !empty($_POST["apellido_alumno"]) && !empty($_POST["fecha_nacimiento_alumno"]) && !empty($_POST["apellido_alumno_original"]) && !empty($_POST["apellido_alumno_original"])){
+       
+        $nombre_original = $_POST["nombre_alumno_original"];
+        $apellido_original = $_POST["apellido_alumno_original"];
+        $dni_alumno = $_POST["dni_alumno"];
+        $nombre_alumno = $_POST["nombre_alumno"];
+        $apellido_alumno = $_POST["apellido_alumno"];
+        $fecha_nacimiento_alumno = $_POST["fecha_nacimiento_alumno"];
+        
+        $contenedor = "UPDATE alumno SET dni_alumno=:dni_alumno,nombre_alumno=:nombre_alumno,apellido_alumno=:apellido_alumno,fecha_nacimiento_alumno=:fecha_nacimiento_alumno WHERE nombre_alumno=:nombre_alumno_original and apellido_alumno = :apellido_alumno_original";
+        $alumno = $connection -> prepare($contenedor);
+    
+        $alumno -> bindParam(":dni_alumno",$dni_alumno);
+        $alumno -> bindParam(":nombre_alumno",$nombre_alumno);
+        $alumno -> bindParam(":apellido_alumno",$apellido_alumno);
+        $alumno -> bindParam(":fecha_nacimiento_alumno",$fecha_nacimiento_alumno);
+        $alumno -> bindParam(":nombre_alumno_original",$nombre_original);
+        $alumno -> bindParam(":apellido_alumno_original",$apellido_alumno);
+        $alumno -> execute();
+
+        header("location:modificacion_alumno.php");
+    }
+?>
