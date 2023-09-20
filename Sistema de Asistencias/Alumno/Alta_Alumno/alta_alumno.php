662d0485e93b9b4cd9ef7aab1d9e613eb519c61b
Pablo088 <129628068+Pablo088@users.noreply.github.com>
2023-09-20 19:52:58 -0300

    alta_alumno.php

--- Sistema de Asistencias/Alumno/Alta_Alumno/alta_alumno.php
+++ Sistema de Asistencias/Alumno/Alta_Alumno/alta_alumno.php
@@ -39,12 +39,14 @@
         echo" ";
     }
 
-    $dni_alumno = $_POST["dni_alumno"];
-    $nombre_alumno = $_POST["nombre_alumno"];
-    $apellido_alumno = $_POST["apellido_alumno"];
-    $fecha_nacimiento_alumno = $_POST["fecha_nacimiento_alumno"];
+    
 
-    if(($dni_alumno && $nombre_alumno && $apellido_alumno && $fecha_nacimiento_alumno)<>null){
+    if(!empty($_POST["dni_alumno"]) && !empty($_POST["nombre_alumno"]) && !empty($_POST["apellido_alumno"]) && !empty($_POST["fecha_nacimiento_alumno"])){
+        $dni_alumno = $_POST["dni_alumno"];
+        $nombre_alumno = $_POST["nombre_alumno"];
+        $apellido_alumno = $_POST["apellido_alumno"];
+        $fecha_nacimiento_alumno = $_POST["fecha_nacimiento_alumno"];
+        
         $contenedor = "INSERT INTO alumno (dni_alumno,nombre_alumno,apellido_alumno,fecha_nacimiento_alumno) values(:dni_alumno,:nombre_alumno,:apellido_alumno,:fecha_nacimiento_alumno)";
         $alumno = $connection -> prepare($contenedor);
         
