dcf91f940ebb31b5f5bf2bd89acdd6dd6d1460d7
Pablo088 <129628068+Pablo088@users.noreply.github.com>
2023-09-20 15:05:10 -0300

    alta_alumno.php

--- Sistema de Asistencias/Alumno/Alta_Alumno/alta_alumno.php
+++ Sistema de Asistencias/Alumno/Alta_Alumno/alta_alumno.php
@@ -1,19 +1,56 @@
-<?php
-    require_once('./BD/conexion.php');
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agregar Alumno</title>
+</head>
+<body>
+    <form action="alta_alumno.php" method="post">
+        <div id="contenedor">
+            <input type="text" name="dni_alumno" placeholder="DNI">
+            <input type="text" name="nombre_alumno" placeholder="Nombre">
+            <input type="text" name="apellido_alumno" placeholder="Apellido">
+            <input type="text" name="fecha_nacimiento_alumno" placeholder="Fecha de Nacimiento">
+            <input type="submit" value="Agregar">
+        </div>
+    </form>
+</body>
+</html>
 
+
+<?php
+    require_once('../../../SistemaDeAsistencias/BD/conexion.php');
+
+    $dni_alumno = $_POST["dni_alumno"];
+    $nombre_alumno = $_POST["nombre_alumno"];
+    $apellido_alumno = $_POST["apellido_alumno"];
+    $fecha_nacimiento_alumno = $_POST["fecha_nacimiento_alumno"];
+
+    if(($dni_alumno && $nombre_alumno && $apellido_alumno && $fecha_nacimiento_alumno)<>null){
+        $contenedor = "INSERT INTO alumno (dni_alumno,nombre_alumno,apellido_alumno,fecha_nacimiento_alumno) values(:dni_alumno,:nombre_alumno,:apellido_alumno,:fecha_nacimiento_alumno)";
+        $alumno = $connection -> prepare($contenedor);
+        
+        $alumno -> bindParam(":dni_alumno",$dni_alumno);
+        $alumno -> bindParam(":nombre_alumno",$nombre_alumno);
+        $alumno -> bindParam(":apellido_alumno",$apellido_alumno);
+        $alumno -> bindParam(":fecha_nacimiento_alumno",$fecha_nacimiento_alumno);
+        $alumno -> execute();
+    }
     $listadoAlumnos = "Select * from alumno";
     $preparo = $connection -> prepare($listadoAlumnos);
     $preparo -> execute();
     $alumnos = $preparo ->fetchAll();
 
     foreach($alumnos as $listado){
+        echo("<br>");
         echo($listado["dni_alumno"]);
-        echo"<br> ";
+        echo" ";
         echo($listado["nombre_alumno"]);
-        echo"<br> ";
+        echo" ";
         echo($listado["apellido_alumno"]);
-        echo"<br> ";
+        echo" ";
         echo($listado["fecha_nacimiento_alumno"]);
-        echo"<br> ";
+        echo" ";
     }
 ?>
