d674cd3ce7d2863c37e22b1ee3bb80b3e181d734
Pablo088 <129628068+Pablo088@users.noreply.github.com>
2023-09-22 15:09:44 -0300

    alta_alumno.php

--- Sistema de Asistencias/Alumno/Alta_Alumno/alta_alumno.php
+++ Sistema de Asistencias/Alumno/Alta_Alumno/alta_alumno.php
@@ -4,20 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Agregar Alumno</title>
+    <link rel="stylesheet" href="/bootstrap/bootstrap-5.3.2-dist/css/bootstrap.min.css">
 </head>
 <body>
+    <h1 style="display: flex; justify-content:center;">Ingresá el nombre y apellido del alumno que queres agregar</h1>
     <form action="alta_alumno.php" method="post">
-        <div id="contenedor">
+        <div id="contenedor" style="display: flex;justify-content: center;" class="mt-3">
             <input type="text" name="dni_alumno" placeholder="DNI">
             <input type="text" name="nombre_alumno" placeholder="Nombre">
             <input type="text" name="apellido_alumno" placeholder="Apellido">
             <input type="text" name="fecha_nacimiento_alumno" placeholder="Fecha de Nacimiento">
-            <input type="submit" value="Agregar">
+            <input type="submit" value="Agregar" class="btn btn-outline-primary">
         </div>
     </form>
-      <a href="/pagina_principal.html">Volver al inicio</a>
+    <a href="/pagina_principal.html"><button class="btn btn-outline-secondary">Volver a inicio</button></a>
 </body>
 </html>
+
 
 <?php
     require_once('../../../SistemaDeAsistencias/BD/conexion.php');
@@ -38,6 +41,8 @@
         echo($listado["fecha_nacimiento_alumno"]);
         echo" ";
     }
+
+    
 
     if(!empty($_POST["dni_alumno"]) && !empty($_POST["nombre_alumno"]) && !empty($_POST["apellido_alumno"]) && !empty($_POST["fecha_nacimiento_alumno"])){
         $dni_alumno = $_POST["dni_alumno"];
