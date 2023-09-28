dad6006e653429ec459a62cdda5ef5ae4509ea39
RomanColetti <129554236+RomanColetti@users.noreply.github.com>
2023-09-28 20:34:07 -0300

    Add files via upload

--- Sistema de Asistencias/Documentacion/sistema_asistencia/sistema_asistencia.sql
+++ Sistema de Asistencias/Documentacion/sistema_asistencia/sistema_asistencia.sql
@@ -1,95 +1,60 @@
-LastSessions<|||>1<|||>Laragon.MySQL
-LastActiveSession<|||>1<|||>Laragon.MySQL
-ColWidths_connform.ListSessions<|||>1<|||>163,50,50,50,50,50,10
-ColsVisible_connform.ListSessions<|||>1<|||>0,1,4,6
-ColPositions_connform.ListSessions<|||>1<|||>0,1,2,3,4,5,6
-ColSort_connform.ListSessions<|||>1<|||>0,0
-MainWinOnMonitor<|||>3<|||>0
-ColWidths_MainForm.ListDatabases<|||>1<|||>150,80,50,50,50,50,50,50,50,50,120
-ColsVisible_MainForm.ListDatabases<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10
-ColPositions_MainForm.ListDatabases<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10
-ColSort_MainForm.ListDatabases<|||>1<|||>0,0
-ColWidths_MainForm.ListVariables<|||>1<|||>160,200,428
-ColsVisible_MainForm.ListVariables<|||>1<|||>0,1,2
-ColPositions_MainForm.ListVariables<|||>1<|||>0,1,2
-ColSort_MainForm.ListVariables<|||>1<|||>0,0
-ColWidths_MainForm.ListStatus<|||>1<|||>160,428,100,100
-ColsVisible_MainForm.ListStatus<|||>1<|||>0,1,2,3
-ColPositions_MainForm.ListStatus<|||>1<|||>0,1,2,3
-ColSort_MainForm.ListStatus<|||>1<|||>0,0
-ColWidths_MainForm.ListProcesses<|||>1<|||>70,80,80,80,80,50,50,300
-ColsVisible_MainForm.ListProcesses<|||>1<|||>0,1,2,3,4,5,6,7
-ColPositions_MainForm.ListProcesses<|||>1<|||>0,1,2,3,4,5,6,7
-ColSort_MainForm.ListProcesses<|||>1<|||>0,1
-ColWidths_MainForm.ListCommandStats<|||>1<|||>120,100,100,100,368
-ColsVisible_MainForm.ListCommandStats<|||>1<|||>0,1,2,3,4
-ColPositions_MainForm.ListCommandStats<|||>1<|||>0,1,2,3,4
-ColSort_MainForm.ListCommandStats<|||>1<|||>1,1
-ColWidths_MainForm.ListTables<|||>1<|||>120,70,70,120,120,70,100,50,70,70,70,70,70,90,120,70,70,70,50
-ColsVisible_MainForm.ListTables<|||>1<|||>0,1,2,3,4,5,6,18
-ColPositions_MainForm.ListTables<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18
-ColSort_MainForm.ListTables<|||>1<|||>0,0
-MainWinWidth<|||>3<|||>993
-MainWinHeight<|||>3<|||>525
-CreateDbCollation<|||>1<|||>utf8mb4_0900_ai_ci
-ColWidths_frmTableEditor.listColumns<|||>1<|||>42,100,152,90,98,121,145,117,130,139,100,100
-ColsVisible_frmTableEditor.listColumns<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11
-ColPositions_frmTableEditor.listColumns<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11
-ColSort_frmTableEditor.listColumns<|||>1<|||>-1,0
-ColWidths_frmTableEditor.treeIndexes<|||>1<|||>919,100,80,120
-ColsVisible_frmTableEditor.treeIndexes<|||>1<|||>0,1,2,3
-ColPositions_frmTableEditor.treeIndexes<|||>1<|||>0,1,2,3
-ColSort_frmTableEditor.treeIndexes<|||>1<|||>-1,0
-ColWidths_frmTableEditor.listForeignKeys<|||>1<|||>61,80,100,80,80,80
-ColsVisible_frmTableEditor.listForeignKeys<|||>1<|||>0,1,2,3,4,5
-ColPositions_frmTableEditor.listForeignKeys<|||>1<|||>0,1,2,3,4,5
-ColSort_frmTableEditor.listForeignKeys<|||>1<|||>-1,0
-ColWidths_frmTableEditor.listCheckConstraints<|||>1<|||>200,281
-ColsVisible_frmTableEditor.listCheckConstraints<|||>1<|||>0,1
-ColPositions_frmTableEditor.listCheckConstraints<|||>1<|||>0,1
-ColSort_frmTableEditor.listCheckConstraints<|||>1<|||>-1,0
-MainWinMaximized<|||>3<|||>1
-FileDialogEncoding_MainForm<|||>3<|||>0
-SQLFile0<|||>1<|||>C:\Users\rndrz\Downloads\sistema_asistencia.sql
-Servers\Laragon.MySQL\SessionCreated<|||>1<|||>2022-09-09 20:36:07
-Servers\Laragon.MySQL\Host<|||>1<|||>127.0.0.1
-Servers\Laragon.MySQL\WindowsAuth<|||>3<|||>0
-Servers\Laragon.MySQL\CleartextPluginEnabled<|||>3<|||>0
-Servers\Laragon.MySQL\User<|||>1<|||>root
-Servers\Laragon.MySQL\Password<|||>1<|||>2
-Servers\Laragon.MySQL\LoginPrompt<|||>3<|||>0
-Servers\Laragon.MySQL\Port<|||>1<|||>3306
-Servers\Laragon.MySQL\NetType<|||>3<|||>0
-Servers\Laragon.MySQL\Compressed<|||>3<|||>0
-Servers\Laragon.MySQL\LocalTimeZone<|||>3<|||>0
-Servers\Laragon.MySQL\QueryTimeout<|||>3<|||>30
-Servers\Laragon.MySQL\KeepAlive<|||>3<|||>20
-Servers\Laragon.MySQL\FullTableStatus<|||>3<|||>1
-Servers\Laragon.MySQL\Databases<|||>1<|||>
-Servers\Laragon.MySQL\Library<|||>1<|||>libmysql.dll
-Servers\Laragon.MySQL\Comment<|||>1<|||>
-Servers\Laragon.MySQL\StartupScriptFilename<|||>1<|||>
-Servers\Laragon.MySQL\TreeBackground<|||>3<|||>536870911
-Servers\Laragon.MySQL\SshExecutable<|||>1<|||>plink.exe
-Servers\Laragon.MySQL\SSHtunnelHost<|||>1<|||>
-Servers\Laragon.MySQL\SSHtunnelHostPort<|||>3<|||>22
-Servers\Laragon.MySQL\SSHtunnelUser<|||>1<|||>
-Servers\Laragon.MySQL\SSHtunnelPassword<|||>1<|||>2
-Servers\Laragon.MySQL\SSHtunnelTimeout<|||>3<|||>4
-Servers\Laragon.MySQL\SSHtunnelPrivateKey<|||>1<|||>
-Servers\Laragon.MySQL\SSHtunnelPort<|||>3<|||>3307
-Servers\Laragon.MySQL\SSL_Active<|||>3<|||>0
-Servers\Laragon.MySQL\SSL_Key<|||>1<|||>
-Servers\Laragon.MySQL\SSL_Cert<|||>1<|||>
-Servers\Laragon.MySQL\SSL_CA<|||>1<|||>
-Servers\Laragon.MySQL\SSL_Cipher<|||>1<|||>
-Servers\Laragon.MySQL\IgnoreDatabasePattern<|||>1<|||>
-Servers\Laragon.MySQL\LogFileDdl<|||>3<|||>0
-Servers\Laragon.MySQL\LogFileDml<|||>3<|||>0
-Servers\Laragon.MySQL\LogFilePath<|||>1<|||>C:\Users\Administrator\AppData\Roaming\HeidiSQL\Logs\%session\%db\%y%m%d.sql
-Servers\Laragon.MySQL\RefusedCount<|||>3<|||>3
-Servers\Laragon.MySQL\ServerVersionFull<|||>1<|||>8.0.30 - MySQL Community Server - GPL
-Servers\Laragon.MySQL\ConnectCount<|||>3<|||>9
-Servers\Laragon.MySQL\ServerVersion<|||>3<|||>80030
-Servers\Laragon.MySQL\LastConnect<|||>1<|||>2023-09-20 11:06:57
-Servers\Laragon.MySQL\lastUsedDB<|||>1<|||>sistema_asistencia
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versión del servidor:         8.0.30 - MySQL Community Server - GPL
+-- SO del servidor:              Win64
+-- HeidiSQL Versión:             12.1.0.6537
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Volcando estructura de base de datos para sistema_asistencia
+CREATE DATABASE IF NOT EXISTS `sistema_asistencia` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `sistema_asistencia`;
+
+-- Volcando estructura para tabla sistema_asistencia.alumno
+CREATE TABLE IF NOT EXISTS `alumno` (
+  `dni_alumno` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `nombre_alumno` text,
+  `apellido_alumno` text,
+  `fecha_nacimiento_alumno` text,
+  PRIMARY KEY (`dni_alumno`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla sistema_asistencia.asistencia
+CREATE TABLE IF NOT EXISTS `asistencia` (
+  `dni_alumno` varchar(50) DEFAULT NULL,
+  `cantidad_asistencias` int DEFAULT NULL,
+  `fecha_hora` timestamp NULL DEFAULT NULL,
+  KEY `alumno_asistencia` (`dni_alumno`),
+  CONSTRAINT `alumno_asistencia` FOREIGN KEY (`dni_alumno`) REFERENCES `alumno` (`dni_alumno`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla sistema_asistencia.profesor
+CREATE TABLE IF NOT EXISTS `profesor` (
+  `dni_profesor` varchar(50) NOT NULL,
+  `nombre_profesor` text,
+  `apellido_profesor` text,
+  `fecha_nacimiento_profesor` text,
+  `materia` text,
+  PRIMARY KEY (`dni_profesor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
