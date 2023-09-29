64bda60e2375210e1e89c20a2e4290ecb6b1186a
RomanColetti <129554236+RomanColetti@users.noreply.github.com>
2023-09-28 21:02:29 -0300

    Delete Sistema de Asistencias directory

--- Sistema de Asistencias/BD/conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-$host = "localhost";
-$dbname = "sistema_asistencia";
-$username = "root";
-$password = null;
-$connection = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-?>
