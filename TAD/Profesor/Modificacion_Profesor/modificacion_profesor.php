09f5eb019a9aef62a73a22b008d6b59f1e65e5a4
Pablo088 <129628068+Pablo088@users.noreply.github.com>
2023-10-10 21:34:50 -0300

    modificacion_profesor.php

--- TAD/Profesor/Modificacion_Profesor/modificacion_profesor.php
+++ TAD/Profesor/Modificacion_Profesor/modificacion_profesor.php
@@ -29,46 +29,16 @@
 <?php
     require_once('../../../SistemaDeAsistencias/BD/conexion.php');
 
-    $listadoProfesores = "Select * from profesor";
-    $preparo = $connection -> prepare($listadoProfesores);
-    $preparo -> execute();
-    $profesores = $preparo ->fetchAll();
-
-    foreach($profesores as $listado){
-        echo("<br>");
-        echo($listado["dni_profesor"]);
-        echo" ";
-        echo($listado["nombre_profesor"]);
-        echo" ";
-        echo($listado["apellido_profesor"]);
-        echo" ";
-        echo($listado["fecha_nacimiento_profesor"]);
-        echo" ";
-        echo($listado["materia"]);
-        echo" ";
-    }
-
     if(!empty($_POST["dni_profesor"]) && !empty($_POST["nombre_profesor"]) && !empty($_POST["apellido_profesor"]) && !empty($_POST["fecha_nacimiento_profesor"]) && !empty($_POST["apellido_profesor_original"]) && !empty($_POST["materia"])){
        
-        $nombre_original = $_POST["nombre_profesor_original"];
-        $apellido_original = $_POST["apellido_profesor_original"];
         $dni_profesor = $_POST["dni_profesor"];
         $nombre_profesor = $_POST["nombre_profesor"];
         $apellido_profesor = $_POST["apellido_profesor"];
         $fecha_nacimiento_profesor = $_POST["fecha_nacimiento_profesor"];
         $materia = $_POST["materia"];
         
-        $contenedor = "UPDATE profesor SET dni_profesor=:dni_profesor,nombre_profesor=:nombre_profesor,apellido_profesor=:apellido_profesor,fecha_nacimiento_profesor=:fecha_nacimiento_profesor,materia=:materia WHERE nombre_profesor=:nombre_profesor_original and apellido_profesor = :apellido_profesor_original";
-        $profesor = $connection -> prepare($contenedor);
-    
-        $profesor -> bindParam(":dni_profesor",$dni_profesor);
-        $profesor -> bindParam(":nombre_profesor",$nombre_profesor);
-        $profesor -> bindParam(":apellido_profesor",$apellido_profesor);
-        $profesor -> bindParam(":fecha_nacimiento_profesor",$fecha_nacimiento_profesor);
-        $profesor -> bindParam(":materia",$materia);
-        $profesor -> bindParam(":nombre_profesor_original",$nombre_original);
-        $profesor -> bindParam(":apellido_profesor_original",$apellido_profesor);
-        $profesor -> execute();
+        $contenedor = "UPDATE profesor SET dni_profesor='$dni_profesor',nombre_profesor='$nombre_profesor',apellido_profesor='$apellido_profesor',fecha_nacimiento_profesor='$fecha_nacimiento_profesor',materia='$materia'";
+        $profesor = mysqli_query($connection,$contenedor);
 
         header("location:modificacion_profesor.php");
     }
