61a0e7cc783ac46c511b94e82a2cfb43dfc259ca
Pablo088 <129628068+Pablo088@users.noreply.github.com>
2023-10-11 17:28:35 -0300

    baja_alumno.php

--- TAD/Alumno/Baja_Alumno/baja_alumno.php
+++ TAD/Alumno/Baja_Alumno/baja_alumno.php
@@ -1,38 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Eliminar Alumno</title>
-    <link rel="stylesheet" href="/bootstrap/bootstrap-5.3.2-dist/css/bootstrap.min.css">
-</head>
-<body class="bg-info bg-opacity-50">
-    <h1 class='mt-3 d-flex justify-content-center'>Ingresá el nombre y apellido del alumno que queres borrar</h1>
-    <form action="baja_alumno.php" method="post">
-        <div class='mt-3 d-flex justify-content-center'>
-            <input type="text" name="nombre_alumno_original" placeholder="Nombre">
-            <input type="text" name="apellido_alumno_original" placeholder="Apellido">
-            <input type="submit" value="Borrar" class="btn btn-primary">
-        </div>
-    </form>
-    <div class="m-4 container d-flex justify-content-center">
-            <a href="/pagina_principal.html"><button class="btn btn-secondary">Inicio</button></a>
-            <a href="/Alumno/pagina_alumno.php"><button class="btn btn-secondary">Atrás</button></a>
-    </div>
-</body>
-</html>
-
 <?php
     require_once('../../../TAD/BD/conexion.php');
-
-    if(!empty($_POST["apellido_alumno_original"]) && !empty($_POST["apellido_alumno_original"])){
-       
-        $nombre_original = $_POST["nombre_alumno_original"];
-        $apellido_original = $_POST["apellido_alumno_original"];
-        
-        $contenedor = "DELETE  FROM alumno WHERE nombre_alumno = :nombre_alumno_original and apellido_alumno = :apellido_alumno_original";
+    if(!empty($_GET["dni"])){
+        $dni_alumno = $_GET["dni"];
+        $contenedor = "DELETE FROM alumno, asistencia where dni_alumno = '$dni_alumno'";
         $alumno = mysqli_query($connection,$contenedor);
 
-        echo "<div class='mt-3 d-flex justify-content-center'>¡El alumno fue eliminado exitosamente!</div>";
+        if($alumno){
+            ?><script> alert("¡El alumno fue eliminado!");
+            location.href ="/Alumno/pagina_alumno.php";</script><?php
+        } 
     }
 ?>
