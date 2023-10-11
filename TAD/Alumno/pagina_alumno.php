1e0686fc581611d3d3739174580b6e5daffb341d
RomanColetti <129554236+RomanColetti@users.noreply.github.com>
2023-10-10 21:16:36 -0300

    Add files via upload

--- TAD/Alumno/pagina_alumno.php
+++ TAD/Alumno/pagina_alumno.php
@@ -7,13 +7,14 @@
     <link rel="stylesheet" href="/bootstrap/bootstrap-5.3.2-dist/css/bootstrap.min.css">
 </head>
 <body style="background-color: rgb(126, 184, 192);">
+    <a href="../pagina_principal.html"><button class="btn btn-secondary">Volver atrás</button></a>
     <h1 style="display: flex; justify-content: center;color: antiquewhite; margin-top: 10px;" >Agregá, eliminá o modificá el alumno que quieras</h1>
     <div class="mt-3" style="display: flex; justify-content: center;align-items: center;">
         <a href="/Alumno/Alta_Alumno/alta_alumno.php"><button class="btn btn-primary btn-lg">Agregar</button></a>
-        <a href="/Alumno/Baja_Alumno/baja_alumno.php"><button class="btn btn-primary btn-lg">Eliminar</button></a>
-        <a href="/Alumno/Modificacion_Alumno/modificacion_alumno.php"><button class="btn btn-primary btn-lg">Modificar</button></a>
+        <!---<a href="/Alumno/Baja_Alumno/baja_alumno.php"><button class="btn btn-primary btn-lg">Eliminar</button></a>
+        <a href="/Alumno/Modificacion_Alumno/modificacion_alumno.php"><button class="btn btn-primary btn-lg">Modificar</button></a>-->
     </div>
-    <a href="../pagina_principal.html"><button class="btn btn-secondary">Volver atrás</button></a>
+
 </body>
 </html>
 
@@ -32,7 +33,11 @@
         echo($listado["apellido_alumno"]);
         echo" ";
         echo($listado["fecha_nacimiento_alumno"]);
+        echo"||";
         echo" ";
+        echo "<a href='Modificacion_Alumno/modificacion_alumno.php?id=".$listado['dni_alumno']."'>Modificar</a>";
+        echo "|";
+        echo "<a href='Baja_Alumno/baja_alumno.php'>Eliminar</a>";
         echo"</div>";
     }
 ?>
