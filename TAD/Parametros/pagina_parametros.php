28275ef19c9172f78f71beec038e358ad39e3f5a
Pablo088 <129628068+Pablo088@users.noreply.github.com>
2023-10-13 17:03:12 -0300

    pagina_parametros.php

--- TAD/Parametros/pagina_parametros.php
+++ TAD/Parametros/pagina_parametros.php
@@ -4,11 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Parametros</title>
+    <link rel="stylesheet" href="/bootstrap/bootstrap-5.3.2-dist/css/bootstrap.min.css">
 </head>
 <body>
-    <h1>Parametros</h1>
-    <input type="text">
-    <input type="text">
+    <div class="d-block align-items-center">
+        <h1>Parametros</h1>
+        <div>
+            <input type="text" >
+            <input type="text">
+        </div>
+    </div>
 </body>
 </html>
 <?php
