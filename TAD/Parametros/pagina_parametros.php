7881a8a085217520ca0488cc03f01f8c0ffe212f
Pablo088 <129628068+Pablo088@users.noreply.github.com>
2023-10-12 21:17:04 -0300

    Add files via upload

---
+++ TAD/Parametros/pagina_parametros.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Parametros</title>
+</head>
+<body>
+    <h1>Parametros</h1>
+    <input type="text">
+    <input type="text">
+</body>
+</html>
+<?php
+    require_once("../../TAD/BD/conexion.php");
+
+
+?>
