6d23879e3bd2de7e4c50b59da8e7761df2870718
RomanColetti <129554236+RomanColetti@users.noreply.github.com>
2023-11-09 20:31:31 -0300

    configuracion.php

--- TAD/Parametros/configuracion.php
+++ TAD/Parametros/configuracion.php
@@ -6,14 +6,13 @@
     $BD = new BD();
 
     if (isset($_POST["enviar"])){
-        if (!empty($_POST["cantidad_dias"]) && !empty($_POST["promedio_promocion"]) && !empty($_POST["promedio_regularidad"]) && !empty($_POST["edad_minima"]) && !empty($_POST['tolerancia'])){
+        if (!empty($_POST["cantidad_dias"]) && !empty($_POST["promedio_promocion"]) && !empty($_POST["promedio_regularidad"]) && !empty($_POST["edad_minima"])){
             $cantidad_dias = $_POST["cantidad_dias"];
             $promedio_promocion = $_POST["promedio_promocion"];
             $promedio_regularidad = $_POST["promedio_regularidad"];
             $edad_minima = $_POST["edad_minima"];
-            $tolerancia = $_POST['tolerancia'];
             
-           $parametro = Parametro::modificarParametros($cantidad_dias,$promedio_promocion,$promedio_regularidad,$edad_minima,$tolerancia);
+           $parametro = Parametro::modificarParametros($cantidad_dias,$promedio_promocion,$promedio_regularidad,$edad_minima);
            $contenedor = $BD->Ejecutar($parametro);
            
             if($contenedor){
