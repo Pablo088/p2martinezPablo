eb59ba8fef20f8b530456d7381de59c86db50dcb
RomanColetti <129554236+RomanColetti@users.noreply.github.com>
2023-11-09 20:32:28 -0300

    Parametro.php

--- TAD/Clases/Parametro.php
+++ TAD/Clases/Parametro.php
@@ -9,8 +9,8 @@ class Parametro {
         $parametro = "select * from parametros";
         return $parametro;
     }
-    public static function modificarParametros($cantidad,$promedioPromocion,$promedioRegularidad,$edadMinima,$tolerancia){
-        $parametros = "UPDATE parametros SET cantidad_dias = '$cantidad', promedio_promocion = '$promedioPromocion', promedio_regularidad = '$promedioRegularidad', edad_minima = '$edadMinima',tolerancia = '$tolerancia' ";
+    public static function modificarParametros($cantidad,$promedioPromocion,$promedioRegularidad,$edadMinima){
+        $parametros = "UPDATE parametros SET cantidad_dias = '$cantidad', promedio_promocion = '$promedioPromocion', promedio_regularidad = '$promedioRegularidad', edad_minima = '$edadMinima' ";
         return $parametros;
     }
 }
