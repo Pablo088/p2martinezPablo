c1a26e4b70600abff6c02f7d89571854c8db7c92
RomanColetti <129554236+RomanColetti@users.noreply.github.com>
2023-11-09 20:22:19 -0300

    pagina_principal.php

--- TAD/pagina_principal.php
+++ TAD/pagina_principal.php
@@ -3,6 +3,7 @@
    require_once("../TAD/BD/conexion.php");
    require_once("../TAD/Clases/Alumno.php");
    require_once("../TAD/Clases/Persona.php");
+   require_once("../TAD/Clases/Parametro.php");
    $BD = new BD();
 
 if(isset($_POST["enviar"])){
@@ -14,16 +15,28 @@ if(isset($_POST["enviar"])){
         $alumnos = $contenedor;
         $alumno = $alumnos -> fetch_assoc();
 
+        $getParametro = Parametro::getParametros();
+        $contenedorParametro = $BD -> Ejecutar($getParametro);
+        $parametro = $contenedorParametro -> fetch_assoc();
+
+        $contadorAsistencia = Alumno::contadorAsistencias($dniBuscar);
+        $contenedorAsistencia = $BD -> Ejecutar($contadorAsistencia);
+        $asistencia = $contenedorAsistencia -> fetch_assoc();
+
         if($dniBuscar == $alumno['dni_alumno']){
             date_default_timezone_set(timezoneId:"America/Argentina/Buenos_Aires");
             $fecha_hora = date("Y-m-d H:i");
-
-            $contenedorAsistencia = Alumno::insertarAsistencia($dniBuscar,$fecha_hora);
-            $ejecutarContenedor = $BD -> Ejecutar($contenedorAsistencia);
-            if($ejecutarContenedor){
-                 ?><script> alert("¡Se pudo cargar la asistencia!");
-                 location.href ="pagina_principal.html";</script><?php
-             } 
+            if ($asistencia['count(dni_alumno)'] >= $parametro['cantidad_dias']){
+                echo"<script> alert('El alumno ya alcanzó el tope de asistencias');
+                location.href ='pagina_principal.html';</script>";
+            }else{
+                $contenedorAsistencia = Alumno::insertarAsistencia($dniBuscar,$fecha_hora);
+                $ejecutarContenedor = $BD -> Ejecutar($contenedorAsistencia);
+                if($ejecutarContenedor){
+                     ?><script> alert("¡Se pudo cargar la asistencia!");
+                     location.href ="pagina_principal.html";</script><?php
+                 } 
+            }
         } else{
                 echo"<script> alert('El dni que ingresaste no existe');
                 location.href ='pagina_principal.html';</script>";
@@ -34,4 +47,3 @@ if(isset($_POST["enviar"])){
        }
 }
 ?>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
-
